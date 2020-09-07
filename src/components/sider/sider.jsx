f86d6c19712deb2d7fd8cefe7bf7c21496361b4d
ANM699 <kevin_kuo@126.com>
2020-09-07 22:49:39 +0800

    合并

--- src/components/sider/sider.jsx
+++ src/components/sider/sider.jsx
@@ -27,11 +27,11 @@ class Sider extends Component {
       >
         <div className={styles.logo}>PMS</div>
         <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-          {/* <Menu.Item
+          <Menu.Item
             key="1"
             icon={<UserOutlined />}
             onClick={() => {
-              this.props.history.replace('/project/backlog');
+              this.props.history.replace("/project/backlog");
             }}
           >
             Backlog
