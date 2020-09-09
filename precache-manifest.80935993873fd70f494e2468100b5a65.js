a3425ecce37f86ef2a61b38397022274b041d5ee
@guo.yunxiang <guo.yunxinag@bestv.com.cn>
2020-09-09 10:38:58 +0800

    Updates

---
+++ precache-manifest.80935993873fd70f494e2468100b5a65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89dd97df081957ae1f4f511005279469",
+    "url": "/PMS/index.html"
+  },
+  {
+    "revision": "fb32be151487908b4b3c",
+    "url": "/PMS/static/css/main.99c3b436.chunk.css"
+  },
+  {
+    "revision": "e75a61bab142f2a45aa5",
+    "url": "/PMS/static/js/2.8283a62c.chunk.js"
+  },
+  {
+    "revision": "81ac3023490625ba2b58d590c30a4c24",
+    "url": "/PMS/static/js/2.8283a62c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb32be151487908b4b3c",
+    "url": "/PMS/static/js/main.7df05123.chunk.js"
+  },
+  {
+    "revision": "86826aa726ad83e93bdb",
+    "url": "/PMS/static/js/runtime-main.a2955c9a.js"
+  }
+]);
