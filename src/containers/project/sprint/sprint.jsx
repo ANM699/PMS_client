774238833f07ba0a26d4f60826ef0caaff95f2be
@guo.yunxiang <guo.yunxinag@bestv.com.cn>
2020-09-15 18:05:26 +0800

    0915

--- src/containers/project/sprint/sprint.jsx
+++ src/containers/project/sprint/sprint.jsx
@@ -1,49 +1,72 @@
-import React, { Component } from "react";
-import { Card, Table, Collapse } from "antd";
-import { connect } from "react-redux";
-import moment from "moment";
+import React, { Component } from 'react';
+import { Card, Table, Collapse, Radio } from 'antd';
+import { connect } from 'react-redux';
+import moment from 'moment';
 
 const { Panel } = Collapse;
+const options = [
+  { label: '2周', value: 14 },
+  { label: '3周', value: 21 },
+  { label: '4周', value: 28 },
+];
 
 class Sprint extends Component {
   state = {
     sprints: [],
+    value: null,
   };
 
-  generateSprint = () => {
+  generateSprint = (e) => {
     const { startDate, endDate } = this.props.project;
+    const offset = e.target.value;
     const sprints = [];
-    function calDeadline(start, end) {
+    function calDeadline(start, end, offset) {
       if (moment(start).isAfter(end)) return;
       if (moment(start).isSame(end))
         sprints.push({ startDate: start, endDate: end });
-      let deadline = moment(start).add(9, "days").format("YYYY-MM-DD");
+      let deadline = moment(start).add(offset, 'days').format('YYYY-MM-DD');
       if (moment(deadline).isAfter(end)) deadline = end;
       sprints.push({ startDate: start, endDate: deadline });
-      const nextStart = moment(deadline).add(1, "days").format("YYYY-MM-DD");
-      calDeadline(nextStart, end);
+      const nextStart = moment(deadline).add(1, 'days').format('YYYY-MM-DD');
+      calDeadline(nextStart, end, offset);
     }
-    calDeadline(startDate, endDate);
+    calDeadline(startDate, endDate, offset);
     console.log(sprints);
-    this.setState({ sprints });
+    this.setState({ sprints, value: offset });
   };
 
   render() {
     console.log(this.props.project);
+
+    const radio = (
+      <Radio.Group
+        options={options}
+        onChange={this.generateSprint}
+        value={this.state.value}
+        optionType="button"
+      />
+    );
     return (
-      <Card
-        title="项目周期"
-        extra={<a onClick={this.generateSprint}>生成项目周期</a>}
-      >
-        <Collapse expandIconPosition="right">
-          {this.state.sprints.map((sprint, index) => (
-            <Panel header="This is panel header 1" key={index}>
-              <div>
-                {sprint.startDate}~{sprint.endDate}
-              </div>
-            </Panel>
-          ))}
-        </Collapse>
+      <Card title="项目周期" extra={radio}>
+        {this.state.sprints.length !== 0 && (
+          <Collapse expandIconPosition="right">
+            {this.state.sprints.map((sprint, index) => (
+              <Panel
+                header={sprint.startDate + '~' + sprint.endDate}
+                key={index}
+              >
+                <div>
+                  {moment().isBetween(
+                    sprint.startDate,
+                    sprint.endDate,
+                    null,
+                    '[]'
+                  ) && '当前所处周期'}
+                </div>
+              </Panel>
+            ))}
+          </Collapse>
+        )}
       </Card>
     );
   }
